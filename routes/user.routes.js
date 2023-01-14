7336fee5d771c72b397d4af318ac1bdc9df6d0de
Dapato2 <108306801+Dapato2@users.noreply.github.com>
2023-01-14 10:42:13 -0600

    Prueba-Subí un serviceCard temporal

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,25 @@ const express = require('express');
 const { userHome } = require('../controllers/user.Controller.js');
 const isLoggedIn = require("../middleware/isLoggedIn");
 const router = express.Router();
+const User = require('../models/User.model');
 
 
 router.get("/home",isLoggedIn, userHome);
 
 
+// Crear servicio
+router.get("/createService",isLoggedIn, (req,res,next) => {
+    User.find()
+    .then((user) => {
+
+        res.render("user/service/serviceList",{toons}  )
+    })
+    .catch((error) => next(error))
+});
+
+router.post("/createService",isLoggedIn, (req,res,next) => {
+
+});
 
 
 module.exports = router;
